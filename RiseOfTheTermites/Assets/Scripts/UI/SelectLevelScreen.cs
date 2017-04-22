252750ea2d04ec1bee795ace64acdd3967572eea
Christophe Bernard <x_tox_man@hotmail.com>
2017-04-22 19:19:20 +0200

    Level selection (1lvl) + game transition

--- RiseOfTheTermites/Assets/Scripts/UI/SelectLevelScreen.cs
+++ RiseOfTheTermites/Assets/Scripts/UI/SelectLevelScreen.cs
@@ -3,12 +3,17 @@ using Assets.Scripts.Controllers;
 using Assets.Scripts.Utils;
 using UnityEngine;
 using UnityEngine.UI;
+using Assets.Scripts.Models;
+using System.Collections.Generic;
+using Assets.Scripts.Managers;
 
 namespace Assets.Scripts.UI
 {
     public class SelectLevelScreen : MonoBehaviourSingleton<SelectLevelScreen>, IBuildUi
     {
         public Button BackButton;
+        public GameObject ItemTemplate;
+        public Transform ItemPanel;
 
         // Use this for initialization
         void Start()
@@ -41,7 +46,35 @@ namespace Assets.Scripts.UI
 
         private void RebuildChildren()
         {
+            List<Level> lvls = PrototypeManager.Instance.Levels;
 
+            ItemPanel.ClearChildren();
+
+            for (var index = 0; index < lvls.Count; index++)
+            {
+                var level = lvls[index];
+
+                var newItem = Instantiate(ItemTemplate);
+                
+                //newItem.name = "Item " + upgrade;
+                newItem.transform.SetParent(ItemPanel, false);
+                newItem.SetActive(true);
+
+                //newItem.GetComponentInChildren<Text>().text = (index == 0 ? "Stay a " : "Upgrade to ") + upgrade;
+                var index1 = index;
+                var bt = newItem.GetComponentInChildren<Button>();
+
+                var btn_label = bt.GetComponentInChildren<Text>();
+                btn_label.text = string.Format("Level {0}", index);
+                bt.onClick.AddListener(() =>
+                {
+                    Debug.Log("On level selected");
+                    GameController.Instance.NewGame(index1);
+
+                    MonoBehaviourSingleton<SelectLevelScreen>.Instance.gameObject.SetActive(false);
+                    MonoBehaviourSingleton<GameHud>.Instance.gameObject.SetActive(true);
+                });
+            }
         }
     }
 }
