dee55a9e42360a01c7bc678c3af6d51d600f8a2d
Simon Boigelot <sboigelot@gmail.com>
2017-04-22 06:15:40 +0200

    Protype manager / Sprite Manager / Game Manager / Save Manager & GameController

---
+++ RiseOfTheTermites/Assets/Scripts/Controllers/GameController.cs
@@ -0,0 +1,24 @@
+﻿using Assets.Scripts.Managers;
+using Assets.Scripts.Utils;
+
+namespace Assets.Scripts.Controllers
+{
+    class GameController : MonoBehaviourSingleton<GameController>, IBuildUi
+    {
+        public void Awake()
+        {
+            PrototypeManager.Instance.LoadPrototypes();
+            SaveManager.Instance.LoadProfiles();
+        }
+
+        public void NewGame()
+        {
+            GameManager.Instance.NewGame();
+            BuildUi();
+        }
+
+        public void BuildUi()
+        {
+        }
+    }
+}
