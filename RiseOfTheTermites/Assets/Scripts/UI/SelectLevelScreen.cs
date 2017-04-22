3521bbdd45fab904d051f05098c03d3c574bfd20
Christophe Bernard <x_tox_man@hotmail.com>
2017-04-22 15:29:36 +0200

    UI screen flow - Main view + level select

---
+++ RiseOfTheTermites/Assets/Scripts/UI/SelectLevelScreen.cs
@@ -0,0 +1,47 @@
+﻿using System.Linq;
+using Assets.Scripts.Controllers;
+using Assets.Scripts.Utils;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.UI
+{
+    public class SelectLevelScreen : MonoBehaviourSingleton<SelectLevelScreen>, IBuildUi
+    {
+        public Button BackButton;
+
+        // Use this for initialization
+        void Start()
+        {
+            BackButton.onClick.AddListener(TaskOnClick);
+        }
+
+        void TaskOnClick()
+        {
+            MonoBehaviourSingleton<MainMenuController>.Instance.gameObject.SetActive(true);
+            MonoBehaviourSingleton<SelectLevelScreen>.Instance.gameObject.SetActive(false);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+
+        public void Open()
+        {
+            BuildUi();
+        }
+
+        public void BuildUi()
+        {
+
+            RebuildChildren();
+        }
+
+        private void RebuildChildren()
+        {
+
+        }
+    }
+}
