dac55a0782fe33ba78d9b265a444dd206c471aa7
Simon Boigelot <sboigelot@gmail.com>
2017-04-22 10:49:38 +0200

    Camera controller

---
+++ RiseOfTheTermites/Assets/Scripts/Controllers/CameraController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Controllers
+{
+    public class CameraController : MonoBehaviour
+    {
+        public BoxCollider2D Bounds;
+        private Vector3 mouseOrigin;
+
+        void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                mouseOrigin = Input.mousePosition;
+            }
+
+            if (Input.GetMouseButton(0))
+            {
+                var deltaPixel = Input.mousePosition - mouseOrigin;
+
+                var move = deltaPixel;
+                transform.Translate(-move, Space.Self);
+                transform.position = BoundCameraPostion();
+
+                mouseOrigin = Input.mousePosition;
+            }
+        }
+
+        private Vector3 BoundCameraPostion()
+        {
+            var pos = transform.position;
+            if (Bounds == null)
+                return pos;
+
+            var min = Bounds.bounds.min;
+            var max = Bounds.bounds.max;
+
+            pos = new Vector3(
+                Mathf.Min(max.x, Mathf.Max(min.x, pos.x)),
+                Mathf.Min(max.y, Mathf.Max(min.y, pos.y)),
+                pos.z);
+
+            return pos;
+        }
+    }
+}
