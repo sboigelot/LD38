2483cb1ef3eb4d7ff44c72cab99ce3a494305e6b
Christophe Bernard <x_tox_man@hotmail.com>
2017-04-22 13:22:11 +0200

    UI Wip

---
+++ RiseOfTheTermites/Assets/Scripts/UI/MainMenuController.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq;
+using Assets.Scripts.Controllers;
+using Assets.Scripts.Utils;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+namespace Assets.Scripts.UI
+{
+    public class MainMenuController : MonoBehaviourSingleton<SplashPanelScript>, IBuildUi
+    {
+
+        public void Open()
+        {
+            BuildUi();
+        }
+
+        public void BuildUi()
+        {
+            RebuildChildren();
+        }
+
+        private void RebuildChildren()
+        {
+
+            
+        }
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
